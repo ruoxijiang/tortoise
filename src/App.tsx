b2eed12f030dc5b6abacdaa499fa83ee55f2b84d
gtkrab <gtkrab@mini.local>
2023-04-18 21:26:08 +0800

    Vaction Planner v1

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import "./index.css";
+
+export default function App() {
+  const [waiting, setWaiting] = useState(false);
+  const [destination, setDestination] = useState("");
+  const [vacationLen, setVacationLen] = useState(5);
+  const [randomness, setRandomness] = useState(0.6);
+  const [password, setPassword] = useState("");
+  const [result, setResult] = useState('');
+
+  async function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    try {
+      setPassword('');
+      const response = await fetch("/openai", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ destination, vacationLen, randomness, password }),
+      });
+      const data:{error?: unknown, result?: string} = await response.json();
+      setWaiting(false);
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result as string);
+      setDestination("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+    }
+  }
+
+  return (
+    <div>
+      <main className="main">
+        <img src="/dog.png" className={"icon"} />
+        <h3>Vacation Plan</h3>
+        <form onSubmit={(e)=> {
+          if(waiting){
+            return
+          }
+          setWaiting(true);
+          onSubmit(e)
+        }}>
+          <input
+            type="text"
+            name="destination"
+            placeholder="Enter a destination"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+          <label htmlFor="days">Enter length of vacation</label>
+          <input
+              id="days"
+              type="number"
+              name="days"
+              placeholder="Enter length of vacation"
+              value={vacationLen}
+              onChange={(e) => setVacationLen(parseInt(e.target.value))}
+          />
+          <label htmlFor="randomness">Randomness from 0 - 1</label>
+          <input
+              type="number"
+              id="randomness"
+              name="randomness"
+              placeholder="Randomness from 0 - 1"
+              value={randomness}
+              onChange={(e) => setRandomness(Number.parseFloat(e.target.value))}
+          />
+          <label htmlFor="passKey">Enter password</label>
+          <input
+              type="text"
+              id="passKey"
+              name="passKey"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+          />
+          <input type="submit" value={waiting ? "Generating itinerary" : "Generate itinerary"} />
+        </form>
+        <div className={"result"}>
+          <div dangerouslySetInnerHTML={{ __html: result }} />
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/api/index.ts
@@ -0,0 +1,71 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+import {vacationGenerate} from "./generate";
+import {initEnvs} from "./lib/envs";
+
+export interface Env {
+	OPENAI_API_KEY: string,
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	SENDGRID: KVNamespace;
+	OPENAI_KV: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+}
+const keyTTLSeconds = 300;
+async function optionRequest(request: Request) {
+	return new Response(
+		'',
+		{
+			headers: {
+				'content-type': 'application/json;charset=UTF-8',
+				'Access-Control-Allow-Origin': request.headers.get("Origin") as string,
+				'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+				'Access-Control-Allow-Headers': '*',
+			}
+		}
+	);
+}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		initEnvs(env);
+		if(request.method.toLocaleUpperCase() === 'OPTIONS'){
+			return await optionRequest(request)
+		} else if (request.method.toLocaleUpperCase() === 'POST' && request.url.toString().includes('/sendgrid')){
+			const contentType = request.headers.get('content-type');
+			if((contentType || "").includes('application/json')){
+				const body = await request.text();
+				if(body){
+					await env.SENDGRID.put('sendgrid_events', body, {expirationTtl: keyTTLSeconds});
+					return new Response(body, {
+						headers: {
+							"content-type": "application/json;charset=UTF-8",
+						},
+					});
+				}
+			}
+		} else if(request.url.toString().includes("/openai")) {
+			const pass = await env.OPENAI_KV.get('sendgrid_events');
+			console.log(`Pass: ${pass}`);
+			return await vacationGenerate(request, env)
+		}
+		return new Response(request.headers.get('content-type'), {
+			status: 400,
+		})
+	},
+};
