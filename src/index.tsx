9ead684c3b04ee3f4cbba2646c3d00bf779a9f50
rjiang <rjiang@lazycakestudios.net>
2023-06-28 00:15:10 +0800

    Parse openAI stream code

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {route} from "./router";
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import {createRoot} from "react-dom/client";
 const darkTheme = createTheme({
     palette: {
         mode: 'dark',
     },
 });
-ReactDOM.render(
+const container =document.getElementById('root') as Element;
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
       <ThemeProvider theme={darkTheme}>
           <CssBaseline />
           <RouterProvider router={route} />
       </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
